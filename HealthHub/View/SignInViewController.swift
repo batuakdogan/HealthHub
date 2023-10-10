43a008c4544a2f44c7bf0726a0f13f15cd8e3bde
Batu Akdoğan <bat.gsakd@gmail.com>
2023-10-10 22:15:58 +0300

    Initial commit

---
+++ HealthHub/View/SignInViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  HealthHub
+//
+//  Created by Batu Akdoğan on 10.10.2023.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SignInViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var emailtextSignIn: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextSignIn: UITextField!
+    
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    @IBAction func signInButton(_ sender: Any) {
+        
+        
+        if emailtextSignIn.text != "" && passwordTextSignIn.text != "" {
+            
+            
+            Auth.auth().signIn(withEmail: emailtextSignIn.text!, password: passwordTextSignIn.text!) { authdata, error in
+                if error != nil {
+                    
+                    self.makeAlert(titleInput: "Hata", messageInput: "Email Bulunamadı")
+                    
+                } else {
+                    print("ll")
+                    
+                }
+            }
+            
+            
+            
+        } else {
+            
+            makeAlert(titleInput: "Hata", messageInput: "Email/Şifre?")
+        }
+        
+    }
+    
+    
+    
+    @IBAction func signUpButton(_ sender: Any) {
+        
+        performSegue(withIdentifier: "signUpSegue", sender: nil)
+        
+        
+    }
+    
+    
+    func makeAlert(titleInput : String , messageInput:String) {
+        
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        
+        let okButton = UIAlertAction(title: "Tamam", style: UIAlertAction.Style.default, handler: nil)
+        
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+        
+        
+    }
+
+}
+
---
+++ HealthHub/View/SignUpViewController.swift
@@ -0,0 +1,81 @@
+//
+//  SignUpViewController.swift
+//  HealthHub
+//
+//  Created by Batu Akdoğan on 10.10.2023.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SignUpViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextSignUp: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextSignUp: UITextField!
+    
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    @IBAction func addUserButton(_ sender: Any) {
+        
+        if emailTextSignUp.text != "" && passwordTextSignUp.text != "" {
+            Auth.auth().createUser(withEmail: emailTextSignUp.text!, password: passwordTextSignUp.text!) { (authdata, error) in
+                
+                
+                if error != nil {
+                    
+                    self.makeAlert(titleInput: "Hata", messageInput: error?.localizedDescription ?? "Hata")
+                    
+                    
+                    
+                } else {
+                    
+                    self.performSegue(withIdentifier: "toSignIn", sender: nil)
+                }
+                
+                
+            } 
+            
+           
+            
+            
+            
+        }
+        
+        else {
+            
+            
+            makeAlert(titleInput: "Hata", messageInput: "Email/Şifre?")
+            
+        }
+        
+        
+    }
+    
+    func makeAlert(titleInput : String , messageInput:String) {
+        
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        
+        let okButton = UIAlertAction(title: "Tamam", style: UIAlertAction.Style.default, handler: nil)
+        
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+        
+        
+    }
+    
+
+}
